437dd3524cb17bd2b96d0544899f39bd81cd1f03
Joao Paulo <jpdesenvolvedorweb@gmail.com>
2020-05-26 22:38:02 -0300

    Criacao Classe Client-ClientViewModel

---
+++ Model.Entity/Client.cs
@@ -0,0 +1,18 @@
+﻿namespace Model.Entity
+{
+    class Client
+    {
+        private long idClient { get; set; }
+
+        private string name { get; set; }
+
+        private string cpf { get; set; }
+
+        private string address { get; set; }
+
+        private string telephone { get; set; }
+
+        private int state { get; set; }
+
+    }
+}
---
+++ SistemaFinanceiro/ViewModels/ClientViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaFinanceiro.ViewModels
+{
+    public class ClientViewModel
+    {
+        [Key]
+        private long idClient { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Nome!!!")]
+        [MaxLength(100, ErrorMessage = "Máximo {0} caracteres!!!")]
+        [MinLength(2, ErrorMessage = "Minimo {0} caracteres!!!")]
+        private string name { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Cpf!!!")]
+        [MaxLength(11, ErrorMessage = "Máximo {0} caracteres!!!")]
+        [MinLength(2, ErrorMessage = "Minimo {0} caracteres!!!")]
+        private string cpf { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Endereço!!!")]
+        [MaxLength(50, ErrorMessage = "Máximo {0} caracteres!!!")]
+        [MinLength(2, ErrorMessage = "Minimo {0} caracteres!!!")]
+        private string address { get; set; }
+
+        [Required(ErrorMessage = "Preencha o campo Telefone!!!")]
+        [MaxLength(20, ErrorMessage = "Máximo {0} caracteres!!!")]
+        [MinLength(2, ErrorMessage = "Minimo {0} caracteres!!!")]
+        private string telephone { get; set; }
+
+        private int state { get; set; }
+    }
+}
